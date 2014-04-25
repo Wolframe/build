41acaebfb9c6f509f8f39feeb5f522576476cb43
Andreas Baumann <abaumann@yahoo.com>
2014-04-25 12:33:52 +0200

    remove funny characters from log.txt before adding it to log.xml

--- scripts/master/download_newest_logs.sh
+++ scripts/master/download_newest_logs.sh
@@ -112,7 +112,7 @@ for arch in $archs; do
 		case $STATUS in
 			failed|succeeded)
 				tail -n 25 < $LOG_FILE > /tmp/tail.$$
-				TAIL=`cat /tmp/tail.$$ | sed -e 's~&~\&amp;~g' -e 's~<~\&lt;~g'  -e  's~>~\&gt;~g'`
+				TAIL=`cat /tmp/tail.$$ | tr -dc '[\011\012\015\040-\176]' | sed -e 's~&~\&amp;~g' -e 's~<~\&lt;~g'  -e  's~>~\&gt;~g'`
 				;;
 			*)
 				TAIL=''

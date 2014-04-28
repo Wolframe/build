0cf88e34e7d20aa4bfec2e1764a14891c0670d03
Andreas Baumann <abaumann@yahoo.com>
2014-04-28 13:21:19 +0200

    fixed status checks in update_binaries.sh

--- scripts/master/update_binaries.sh
+++ scripts/master/update_binaries.sh
@@ -46,7 +46,7 @@ OLDIFS=$IFS
 osc results > /tmp/results.$$
 cut -f1-3 $base/../../data/manual_results >> /tmp/results.$$
 while read -r PLATFORM ARCH STATUS; do
-	if test $STATUS != "succeeded" -a $STATUS != "failed" -a $STATUS != 'disabled' -a $STATUS != 'unresolvable' -a $STATUS != 'skip'; then
+	if test "x$STATUS" != "xsucceeded" -a "x$STATUS" != "xfailed" -a "x$STATUS" != 'xdisabled' -a "x$STATUS" != 'xunresolvable' -a "x$STATUS" != 'xskip'; then
 		echo "Not updating binaries now, platform $PLATFORM, arch $ARCH is still building.."
 		exit 0
 	fi
